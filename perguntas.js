204b45d0820ab4c76cb435d6cb2268ee9a412c2d
emilicarol <148370520+emilicarol@users.noreply.github.com>
2024-09-30 10:32:42 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,48 @@
+criaCartao(
+    'Astronomia',
+    'Qual é o menor planeta do Sistema Solar?',
+    'O menor planeta é Mercúrio'
+)
+
+criaCartao(
+    'Astronomia',
+    'O que são cometas?',
+    'São corpos gelado que ao se aproximarem o sol, liberam gases e poeira, formando uma cauda brilhante'
+)
+
+criaCartao(
+    'Astronomia',
+    'Quantas luas tem Júpiter?',
+    '92 luas conhecidas'
+)
+
+criaCartao(
+    'Astronomia',
+    'Quantas fases tem a lua?',
+    '4 fases'
+)
+
+criaCartao(
+    'Astronomia',
+    'Qual é o nome da nossa galáxia?',
+    'Via láctea',
+)
+
+criaCartao(
+    'Astronomia',
+    'O que é uma estrela?',
+    'Uma estrela é uma bola de gás que emite luz e calor',
+)
+
+criaCartao(
+    'Astronomia',
+    'O que causa os eclipses?',
+    'O alinhamento da Terra, da Lua e do Sol'
+)
+
+criaCartao(
+    'Astronomia',
+    'O que é um meteoro?',
+    'Um meteoro é uma rocha ou fragmento de poeira que entra na atmofera da Terra'
+)
+
